007e847d71e5fcf65a2ff1034048f57a1d976658
Will Eaton <weaton@redhat.com>
2025-10-16 09:31:17 -0400

    start building aarch64
    Signed-off-by: Will Eaton <weaton@redhat.com>

--- docker/scripts/cuda/builder/build-gdrcopy.sh
+++ docker/scripts/cuda/builder/build-gdrcopy.sh
@@ -5,9 +5,19 @@ set -Eeuo pipefail
 #
 # Required environment variables:
 # - USE_SCCACHE: whether to use sccache (true/false)
+# Optional environment variables:
+# - TARGETPLATFORM: platform target (linux/arm64 or linux/amd64)
 
 # shellcheck source=/dev/null
 source /usr/local/bin/setup-sccache
+
+# determine architecture for gdrcopy build
+UUARCH=""
+case "${TARGETPLATFORM:-linux/amd64}" in
+  linux/arm64) UUARCH="aarch64" ;;
+  linux/amd64) UUARCH="x64" ;;
+  *) echo "Unsupported TARGETPLATFORM: ${TARGETPLATFORM}" >&2; exit 1 ;;
+esac
 
 git clone https://github.com/NVIDIA/gdrcopy.git
 cd gdrcopy
@@ -15,7 +25,7 @@ cd gdrcopy
 if [ "${USE_SCCACHE}" = "true" ]; then
     export CC="sccache gcc" CXX="sccache g++"
 fi
-PREFIX=/usr/local DESTLIB=/usr/local/lib make lib_install
+ARCH="${UUARCH}" PREFIX=/usr/local DESTLIB=/usr/local/lib make lib_install
 
 cp src/libgdrapi.so.2.* /usr/lib64/
 ldconfig
